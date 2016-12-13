75d8e500c74194122a2fc760d1b53f470967ab33
DIWithers <d.ivy.withers@gmail.com>
2016-12-13 13:28:13 -0500

    add check token function for ease

--- js/mainController.js
+++ js/mainController.js
@@ -20,6 +20,7 @@ ecommerceApp.controller("mainController", function($scope, $http, $location, $co
 				$cookies.put("token", response.data.token);
 				console.log(response.data.token);
 				$cookies.put("username", $scope.username);
+				checkToken();
 				$location.path("/options"); 
 			}
 		}, function errorCallback(response) {
@@ -40,6 +41,7 @@ ecommerceApp.controller("mainController", function($scope, $http, $location, $co
 			if(response.data.success === "userFound") {
 				$cookies.put("token", response.data.token);
 				$cookies.put("username", $scope.username);
+				checkToken();
 				$location.path("/options");
 				$rootScope.loggedIn = true;
 				console.log("Logged In successfully");
